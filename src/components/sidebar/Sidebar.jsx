e075ca84bdc41074e418f5defb059326de4ad4e8
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-05 09:31:09 -0300

    component sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,10 +6,10 @@ import icons from '../../styles/icons';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <Link to="/"> <img src={icons.home} /> </Link>
-      <Link to="/despesa"> <img src={icons.add} /> </Link>
-      <Link to="#"> <img src={icons.config} /> </Link>
-      <Link to="#"> <img src={icons.logout} /> </Link>
+      <Link to="/"> <img className='icon' src={icons.home} /> </Link>
+      <Link to="/despesa"> <img className='icon' src={icons.add} /> </Link>
+      <Link to="#"> <img className='icon' src={icons.config} /> </Link>
+      <Link to="#"> <img className='icon' src={icons.logout} /> </Link>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/home';
 import CadDespesa from './pages/cad-despesa/cad-despesa.jsx';
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
